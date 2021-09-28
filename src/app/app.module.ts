b1a11529ccc3b80195e87b59691cad537512c005
badi3a <badiaabouhdid@gmail.com>
2021-09-28 12:16:41 +0200

    Project V0 (Home page: head + content)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
