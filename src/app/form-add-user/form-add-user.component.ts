5e63eb0f21d2a8bbb8c75db1978b8fc89ff7d46b
badi3a <badiaabouhdid@gmail.com>
2021-10-19 13:07:04 +0200

    séance S5.3

--- src/app/form-add-user/form-add-user.component.ts
+++ src/app/form-add-user/form-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../model/user";
 
 @Component({
   selector: 'app-form-add-user',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-add-user.component.css']
 })
 export class FormAddUserComponent implements OnInit {
-
+  user: User;
   constructor() { }
 
   ngOnInit(): void {
+    this.user =  new User();
+  }
+  save(){
+    //traitement
   }
 
 }
