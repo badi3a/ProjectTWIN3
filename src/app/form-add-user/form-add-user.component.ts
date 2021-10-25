e7b4042e19a5d4172be7e9064bbac97f13c12d09
badi3a <badiaabouhdid@gmail.com>
2021-10-25 11:23:10 +0200

    S6.1

--- src/app/form-add-user/form-add-user.component.ts
+++ src/app/form-add-user/form-add-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../model/user";
+import {SharedDataService} from "../services/shared-data.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-form-add-user',
@@ -8,13 +10,17 @@ import {User} from "../model/user";
 })
 export class FormAddUserComponent implements OnInit {
   user: User;
-  constructor() { }
 
+  constructor(private data:SharedDataService, private router: Router) { }
   ngOnInit(): void {
     this.user =  new User();
+
   }
   save(){
     //traitement
+    this.user.accountCategory='Customer';
+    this.data.list.push(this.user)
+    this.router.navigate(['/user'])
   }
 
 }
