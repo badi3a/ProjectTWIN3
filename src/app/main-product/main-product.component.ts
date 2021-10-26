6489c2a14e8ae016cf8494093dff27901a613899
badi3a <badiaabouhdid@gmail.com>
2021-10-26 11:07:19 +0200

    S7.1

--- src/app/main-product/main-product.component.ts
+++ src/app/main-product/main-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../model/product";
 
 @Component({
   selector: 'app-main-product',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-product.component.css']
 })
 export class MainProductComponent implements OnInit {
-
+  listProduct: Product[];
   constructor() { }
 
   ngOnInit(): void {
+    this.listProduct = [
+      { id: '12',
+        title: 'T-Shirt 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis',
+        quantity: 10,
+        price: 15,
+        nbrLike: 0,
+        picture: 'assets/t shirt 1.jpg'
+      },
+      { id: '12',
+        title: 'T-Shirt 2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis',
+        quantity: 10,
+        price: 25,
+        nbrLike: 0,
+        picture: 'assets/t shirt 2.jpg'
+      },
+      { id: '12',
+        title: 'T-Shirt 3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis',
+        quantity: 0,
+        price: 100,
+        nbrLike: 0,
+        picture: 'assets/t shirt 1.jpg'
+      }
+    ]
   }
 
+  like(product: Product){
+    product.nbrLike++;
+    console.log(this.listProduct)
+  }
 }
