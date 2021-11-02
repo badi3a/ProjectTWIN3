121fc9868750d5fa999d3126ab2e3c4cdfdb7755
badi3a <badiaabouhdid@gmail.com>
2021-11-02 11:41:41 +0100

    S8.2

---
+++ src/app/app-product/app-product.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+
+
+
+@NgModule({
+  declarations: [
+    DetailProductComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppProductModule { }
---
+++ src/app/app-product/detail-product/detail-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-product',
+  templateUrl: './detail-product.component.html',
+  styleUrls: ['./detail-product.component.css']
+})
+export class DetailProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
